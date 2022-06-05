3da5d624c0fd3bdfd2a584d777b072d3bbe14a6f
David Ongaro <davidongaroh@gmil.com>
2022-06-05 10:44:27 +0300

    commit tree 813ec9a0dc0fade65fc18fbce414a178a7954844

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+let namesArray=["Guadalupe", "Ollie", "Aki"]
+function writeCards(namesArray,eventNai){
+    let eventcards=[]
+    for (let i=0; i<namesArray.length; i++) {
+    eventcards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventNai} gift!`);
+    debugger;
+}
+return eventcards;
+}
+function countdown(myInt){
+    let startingNumber=myInt
+
+        while(startingNumber>0); {
+            console.log(startingNumber);
+            startingNumber--;
+        
+    }
+    console.log(startingNumber)
+}
